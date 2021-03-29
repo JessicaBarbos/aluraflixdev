0272bf1eeba9a041facb1330c6491ac6f524bf2b
Jessica Barbosa <jessica@fho.edu.br>
2021-03-29 15:21:40 -0300

    Add function addMovie

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,15 @@
-let moviesList = [];
+// let moviesList = [];
 
-moviesList.push("https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg");
+// moviesList.push("https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg");
 
-moviesList.push("https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg");
+// moviesList.push("https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg");
 
-moviesList.push("https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg");
+// moviesList.push("https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg");
 
-for(let i = 0; i < moviesList.length; i++){
-  document.write("<img src=" + moviesList[i] + "/>");
-}
+// for(let i = 0; i < moviesList.length; i++){
+//   document.write("<img src=" + moviesList[i] + "/>");
+// }
+
+function addMovie(){
+    console.log("clicou")
+  }
